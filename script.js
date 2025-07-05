716d3eaeb247ec694fc26070458aa370348eb9ea
Tejsvi sharma <tejsvis2@gmail.com>
2025-07-05 11:59:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get references to the HTML elements
+    const helloButton = document.getElementById('helloButton');
+    const initialScreen = document.getElementById('initialScreen');
+    const digitalWorldScreen = document.getElementById('digitalWorldScreen');
+    const imag = document.getElementById('imag');
+    const word = document.getElementById('hello-world-text');
+    const dot = document.getElementById('dot');
+    // Add an event listener to the button
+    helloButton.addEventListener('click', () => {
+
+        // 1. Start the transition to hide the initial screen.
+        initialScreen.classList.add('hidden');
+        initialScreen.style.setProperty("opacity", 0);
+        initialScreen.setAttribute('aria-hidden', 'true');
+        imag.style.cssText = "scale:1";
+        imag.playbackRate = 0.5;
+        word.style.cssText = "scale:5;";
+        // imag.load();
+        // imag.play();
+        // dot.style.cssText= "z-index: 1;"; 
+
+
+        // 3. Activate the digital world screen.
+        digitalWorldScreen.classList.add('active');
+        digitalWorldScreen.setAttribute('aria-hidden', 'false');
+    });
+});
